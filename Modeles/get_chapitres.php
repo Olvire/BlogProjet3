d04546fe6317f9682bebc30e39292e4497f34e58
Jean Forteroche <jean.forteroche@outlook.fr>
2017-07-18 14:44:28 +0200

    Modif références à la BDD

--- Modeles/get_chapitres.php
+++ Modeles/get_chapitres.php
@@ -5,12 +5,12 @@ function get_chapitres($offset, $limit)
     $offset = (int) $offset;
     $limit = (int) $limit;
         
-    $req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT :offset, :limit');
+    $req = $bdd->prepare('SELECT id_chap, titre_chap, auteur_chap, contenu_chap, DATE_FORMAT(date_chap, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM chapitres ORDER BY date_chap DESC LIMIT :offset, :limit');
     $req->bindParam(':offset', $offset, PDO::PARAM_INT);
     $req->bindParam(':limit', $limit, PDO::PARAM_INT);
     $req->execute();
     $billets = $req->fetchAll();
     
     
-    return $billets;
+    return $chapitres;
 }
