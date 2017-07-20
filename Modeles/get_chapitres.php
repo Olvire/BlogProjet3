f70776107e214e17115de62e9aa1959d841945b6
Jean Forteroche <jean.forteroche@outlook.fr>
2017-07-20 15:14:02 +0200

    Ajout point d'arrets sur get_chapitres.php

--- Modeles/get_chapitres.php
+++ Modeles/get_chapitres.php
@@ -2,7 +2,7 @@
 function get_chapitres()
 {
     global $bdd;
-    $chapitres = "contenu_chap, titre_chap, date-creation_fr";
+    $chapitres = "titre_chap, date-creation_fr, contenu_chap";
         
     $req = $bdd->prepare('SELECT id_chap, titre_chap, auteur_chap, contenu_chap, DATE_FORMAT(date_chap, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM chapitres ORDER BY date_chap DESC LIMIT 0,3');
     $req->execute();
