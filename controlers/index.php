2954b4f864bd74b57e1b739c552e797a3c3ddcaa
Jean Forteroche <jean.forteroche@outlook.fr>
2017-07-20 10:30:29 +0200

    Modif du controler de l'accueil

--- controlers/index.php
+++ controlers/index.php
@@ -3,10 +3,4 @@
 include_once('Modeles/get_chapitres.php');
 $chapitres = get_chapitres(0, 3);
 
-foreach($chapitres as $cle => $chapitre)
-{
-    $chapitres[$cle]['titre_chap'] = htmlspecialchars($chapitre['titre_chap']);
-    $chapitres[$cle]['contenu_chap'] = nl2br(htmlspecialchars($chapitre['contenu_chap']));
-}
-
 include_once('Vues/index.php');
