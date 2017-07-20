97d970cdd7275caac10972ff4f7dc373fc2c24f7
Jean Forteroche <jean.forteroche@outlook.fr>
2017-07-20 10:08:21 +0200

    Modifications du controlers

--- controlers/index.php
+++ controlers/index.php
@@ -1,16 +1,12 @@
 <?php
 
-// On demande les 5 derniers billets (modèle)
 include_once('Modeles/get_chapitres.php');
-$chapitres = get_chapitres(0, 5);
+$chapitres = get_chapitres(0, 3);
 
-// On effectue du traitement sur les données (contrôleur)
-// Ici, on doit surtout sécuriser l'affichage
 foreach($chapitres as $cle => $chapitre)
 {
     $chapitres[$cle]['titre_chap'] = htmlspecialchars($chapitre['titre_chap']);
     $chapitres[$cle]['contenu_chap'] = nl2br(htmlspecialchars($chapitre['contenu_chap']));
 }
 
-// On affiche la page (vue)
 include_once('Vues/index.php');
